38b84e70e35b2e3722f67a8dd3474d108d6e45f6
Lê Thái Sơn <lethaison04011999@gmail.com>
2023-10-26 21:29:52 +0700

    [add] accesstoken

--- src/server.js
+++ src/server.js
@@ -1,27 +1,25 @@
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import * as dotenv from 'dotenv'
-import express from 'express'
-import connection from './config/connectdb'
-import initAPIRouter from './routers/api'
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+import express from 'express';
+import connection from './config/connectdb';
+import initAPIRouter from './routers/api';
+import task from './controllers/schedulePromotions';
+dotenv.config();
 
-dotenv.config()
+// task.start();
+const app = express();
+app.use(cors({ credentials: true, origin: true }));
+const port = process.env.PORT || 6969;
 
-
-const app = express()
-app.use(cors({ credentials: true, origin: true }))
-const port = process.env.PORT || 6969
-
-app.use(bodyParser.json({ limit: '50mb' }))
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
-
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 // setup router
 
-initAPIRouter(app)
-connection()
+initAPIRouter(app);
+connection();
 
 app.listen(port, () => {
-    return console.log("server running on port: ", port)
-})
-
+  return console.log('server running on port: ', port);
+});
